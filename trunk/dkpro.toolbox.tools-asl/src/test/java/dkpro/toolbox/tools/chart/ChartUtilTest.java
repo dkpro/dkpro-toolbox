749cbb018986dd4af48f632ec6086208af5f52a7
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-10-13 16:02:19 +0000

    Adding chart capabilities.

---
+++ trunk/dkpro.toolbox.tools-asl/src/test/java/dkpro/toolbox/tools/chart/ChartUtilTest.java
@@ -0,0 +1,25 @@
+package dkpro.toolbox.tools.chart;
+
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.ConditionalFrequencyDistribution;
+import dkpro.toolbox.corpus.categorized.CategorizedCorpus;
+import dkpro.toolbox.corpus.categorized.FirstNamesCorpus;
+
+public class ChartUtilTest
+{
+    
+    @Test
+    public void testPrint() throws Exception {
+        
+        CategorizedCorpus corpus = new FirstNamesCorpus();
+        ConditionalFrequencyDistribution<String, String> cfd = new ConditionalFrequencyDistribution<String, String>();
+        for (String category : corpus.getCategories()) {
+            for (String token : corpus.getTokens(category)) {
+                cfd.addSample(category, token.substring(token.length()-1, token.length()));
+            }
+        }
+        
+        ChartUtil.getChart(cfd);
+    }
+}
