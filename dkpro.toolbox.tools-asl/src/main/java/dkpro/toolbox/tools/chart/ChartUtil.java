995d980be4693a8682275dad69a4116ebd385fb1
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-10-13 16:02:19 +0000

    Adding chart capabilities.

---
+++ dkpro.toolbox.tools-asl/src/main/java/dkpro/toolbox/tools/chart/ChartUtil.java
@@ -0,0 +1,40 @@
+package dkpro.toolbox.tools.chart;
+
+import org.jfree.ui.RefineryUtilities;
+
+import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.ConditionalFrequencyDistribution;
+import dkpro.toolbox.corpus.categorized.CategorizedCorpus;
+import dkpro.toolbox.corpus.categorized.FirstNamesCorpus;
+
+public class ChartUtil
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        CategorizedCorpus corpus = new FirstNamesCorpus();
+        ConditionalFrequencyDistribution<String, String> cfd = new ConditionalFrequencyDistribution<String, String>();
+        for (String category : corpus.getCategories()) {
+            for (String token : corpus.getTokens(category)) {
+                cfd.addSample(category, token.substring(token.length()-1, token.length()));
+            }
+        }
+        
+        ChartUtil.getChart(cfd);
+    }
+
+    public static void getChart(ConditionalFrequencyDistribution<String, String> cfd) {
+        FrequencyChart chart = new FrequencyChart("test", cfd);
+        chart.pack();
+        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
+    }
+    
+    public static void safeAsPdf() {
+        
+    }
+    
+    public static void safeAsSvg() {
+        
+    }
+
+}
