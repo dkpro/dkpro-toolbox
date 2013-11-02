0ddeace104acf446e186b7827229c7bc884c289a
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-11-02 21:19:49 +0000

    Make basic classes hashable.

--- trunk/dkpro.toolbox.core-asl/src/main/java/dkpro/toolbox/core/TaggedToken.java
+++ trunk/dkpro.toolbox.core-asl/src/main/java/dkpro/toolbox/core/TaggedToken.java
@@ -58,4 +58,46 @@ public class TaggedToken
     {
     	return token + " (" + tag + ")"; 
     }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((tag == null) ? 0 : tag.hashCode());
+        result = prime * result + ((token == null) ? 0 : token.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TaggedToken other = (TaggedToken) obj;
+        if (tag == null) {
+            if (other.tag != null) {
+                return false;
+            }
+        }
+        else if (!tag.equals(other.tag)) {
+            return false;
+        }
+        if (token == null) {
+            if (other.token != null) {
+                return false;
+            }
+        }
+        else if (!token.equals(other.token)) {
+            return false;
+        }
+        return true;
+    }
 }
