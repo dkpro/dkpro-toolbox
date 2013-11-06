074e7fe727049a115f029d497ae11f31ef632c45
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-11-06 21:28:38 +0000

    New ngram taggers.

---
+++ trunk/dkpro.toolbox.tools-asl/src/main/java/dkpro/toolbox/tools/tagger/util/LimitedQueue.java
@@ -0,0 +1,26 @@
+package dkpro.toolbox.tools.tagger.util;
+
+import java.util.LinkedList;
+
+public class LimitedQueue<E>
+    extends LinkedList<E>
+{
+
+    private static final long serialVersionUID = 1L;
+    private int limit;
+
+    public LimitedQueue(int limit)
+    {
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean add(E o)
+    {
+        super.add(o);
+        while (size() > limit) {
+            super.remove();
+        }
+        return true;
+    }
+}
---
+++ trunk/dkpro.toolbox.tools-asl/src/test/java/dkpro/toolbox/tools/tagger/NGramTaggerTest.java
@@ -0,0 +1,26 @@
+package dkpro.toolbox.tools.tagger;
+
+import org.junit.Test;
+
+import dkpro.toolbox.core.Sentence;
+import dkpro.toolbox.core.TaggedToken;
+import dkpro.toolbox.corpus.analyzed.AnalyzedCorpus;
+import dkpro.toolbox.corpus.analyzed.CorpusManager;
+import dkpro.toolbox.corpus.analyzed.CorpusManager.CorpusName;
+
+public class NGramTaggerTest
+{
+    @Test
+    public void lookupTaggerTest()
+        throws Exception
+    {
+        AnalyzedCorpus corpus = CorpusManager.getCorpus(CorpusName.MobyDick);
+        Sentence sentence = CorpusManager.getSentence(CorpusName.MobyDick);      
+        
+        ToolboxTagger ngramTagger = new NGramTagger(corpus.getSentences(), 1);
+                
+        for (TaggedToken taggedToken : ngramTagger.tag(sentence.getTokens())) {
+            System.out.println(taggedToken);
+        }
+    }
+}
