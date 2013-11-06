bb9c6ba9f8856be1eb21a2e743c465c253f59653
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-11-06 09:32:15 +0000

    Renamed packages.

---
+++ dkpro.toolbox.corpus-asl/src/main/java/dkpro/toolbox/corpus/analyzed/CorpusManager.java
@@ -0,0 +1,97 @@
+package dkpro.toolbox.corpus.analyzed;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dkpro.toolbox.core.Sentence;
+import dkpro.toolbox.corpus.CorpusException;
+import dkpro.toolbox.corpus.SerializedCorpus;
+
+public class CorpusManager
+{
+    public enum CorpusName {
+        MobyDick,
+        SenseAndSensibility,
+        BookOfGenesis,
+        InauguralAddresses,
+        ChatCorpus,
+        WallStreetJournal,
+        PersonalsCorpus,
+        TheManWhoWasThursday,
+        FirstNames
+    }
+    
+    private static Map<CorpusName, AnalyzedCorpus> corpora = null;
+    private static Map<CorpusName, Sentence> sentences = null;
+    
+    private static void initialize()
+    {
+        corpora = new HashMap<CorpusName, AnalyzedCorpus>();
+        sentences = new HashMap<CorpusName, Sentence>();     
+    }
+    
+    private static void load(CorpusName name)
+            throws CorpusException
+    {
+        switch (name) {
+            case MobyDick : corpora.put(CorpusName.MobyDick, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/mobydick/")));
+                addSentence(CorpusName.MobyDick);
+                break;
+            case SenseAndSensibility : corpora.put(CorpusName.SenseAndSensibility, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/sense_sensibility/")));
+                addSentence(CorpusName.SenseAndSensibility);
+                break;
+            case TheManWhoWasThursday: corpora.put(CorpusName.TheManWhoWasThursday, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/thursday/")));
+                addSentence(CorpusName.TheManWhoWasThursday);
+                break;
+            case InauguralAddresses: corpora.put(CorpusName.InauguralAddresses, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/inaugural/")));
+                addSentence(CorpusName.InauguralAddresses);
+                break;
+            case BookOfGenesis: corpora.put(CorpusName.BookOfGenesis, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/genesis/")));
+                addSentence(CorpusName.BookOfGenesis);
+                break;
+            case PersonalsCorpus: corpora.put(CorpusName.PersonalsCorpus, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/personals/")));
+                addSentence(CorpusName.PersonalsCorpus);
+                break;
+            case ChatCorpus: corpora.put(CorpusName.ChatCorpus, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/chat/")));
+                addSentence(CorpusName.ChatCorpus);
+               break;
+            case FirstNames: corpora.put(CorpusName.FirstNames, new AnalyzedCorpus(new SerializedCorpus("classpath:/corpus/names/")));
+                addSentence(CorpusName.FirstNames);
+                break;
+        }
+    }
+    
+    private static void addSentence(CorpusName name)
+            throws CorpusException
+    {
+        sentences.put(name, corpora.get(name).getSentences().get(0));
+    }
+    
+    public static AnalyzedCorpus getCorpus(CorpusName name)
+            throws CorpusException
+    {
+        if (corpora == null) {
+            initialize();
+        }
+        
+        if (!corpora.containsKey(name)) {
+            load(name);
+        }
+        
+        return corpora.get(name);
+    }
+
+    public static Sentence getSentence(CorpusName name)
+            throws CorpusException
+    {
+        if (sentences == null) {
+            initialize();
+        }
+
+        if (!corpora.containsKey(name)) {
+            load(name);
+        }
+        
+        return sentences.get(name);
+    }
+}
---
+++ dkpro.toolbox.corpus-asl/src/main/java/dkpro/toolbox/corpus/special/CmuDict.java
@@ -0,0 +1,138 @@
+package dkpro.toolbox.corpus.special;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import dkpro.toolbox.core.Pronunciation;
+import dkpro.toolbox.core.Sentence;
+import dkpro.toolbox.core.Tag;
+import dkpro.toolbox.core.TaggedToken;
+import dkpro.toolbox.core.Text;
+import dkpro.toolbox.corpus.Corpus;
+import dkpro.toolbox.corpus.CorpusException;
+
+public class CmuDict
+    implements Corpus
+{
+    private Map<String, List<Pronunciation>> dict;
+    
+    public CmuDict()
+            throws CorpusException
+    {
+        
+        dict = new HashMap<String, List<Pronunciation>>();
+        
+        try {
+            URL url = ResourceUtils.resolveLocation("src/main/resources/corpus/cmudict/cmudict");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line = null;
+            while((line = in.readLine()) != null) {
+                String[] parts = line.split(" ");
+                
+                String key = parts[0].toLowerCase();
+                
+                List<String> phonemes = new ArrayList<String>();
+                for (int i=2; i<parts.length; i++) {
+                    phonemes.add(parts[i]);
+                }
+                Pronunciation pronunciation = new Pronunciation(phonemes);
+                
+                List<Pronunciation> pronunciations;
+                if (dict.containsKey(key)) {
+                    pronunciations = dict.get(key);
+                }
+                else {
+                    pronunciations = new ArrayList<Pronunciation>();
+                }
+                pronunciations.add(pronunciation);
+                dict.put(key, pronunciations);
+            }
+        }
+        catch (IOException e) {
+            throw new CorpusException(e);
+        }
+    }
+    
+    public boolean containsPronunciation(String token) {
+        return dict.containsKey(token);
+    }
+    
+    public List<Pronunciation> getPronunciations(String token) {
+        if (dict.containsKey(token)) {
+            return dict.get(token);
+        }
+        else {
+            return null;
+        }
+    }
+    
+    public Pronunciation getMostProbablyPronunciation(String token) {
+        if (dict.containsKey(token)) {
+            return dict.get(token).get(0);
+        }
+        else {
+            return null;
+        } 
+    }
+
+    @Override
+    public Iterable<String> getTokens()
+        throws CorpusException
+    {
+        return dict.keySet();
+    }
+
+    @Override
+    public Iterable<TaggedToken> getTaggedTokens()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Sentence> getSentences()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Tag> getTags()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Text> getTexts()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public String getLanguage()
+    {
+        return "en";
+    }
+
+    @Override
+    public String getName()
+    {
+        return "cmudict";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "CMU pronunciation dictionary";
+    }
+}
