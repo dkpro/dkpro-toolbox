e0432e913a8309d7ea7af24d05173e06e8c0dae7
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-11-12 17:08:06 +0000

    Use local tag mapping.

--- dkpro.toolbox.core-asl/src/main/java/dkpro/toolbox/core/util/TagUtil.java
+++ dkpro.toolbox.core-asl/src/main/java/dkpro/toolbox/core/util/TagUtil.java
@@ -34,7 +34,7 @@ import dkpro.toolbox.core.ToolboxException;
 public class TagUtil
 {
 
-    private static final String pathBase = "classpath:/de/tudarmstadt/ukp/dkpro/core/api/lexmorph/tagset/";
+    private static final String pathBase = "classpath:/tagmapping";
 
     private static MappingProvider posMappingProviderBrown = null;
     private static MappingProvider posMappingProviderPTB = null;
