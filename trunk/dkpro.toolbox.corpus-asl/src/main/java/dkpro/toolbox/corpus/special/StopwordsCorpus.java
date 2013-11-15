725a319d65eea3145db400f4af84e9db71c1b12d
torsten.zesch <torsten.zesch@3e629e7b-27d9-bf99-2158-d804a2f7ebc3>
2013-11-15 20:26:42 +0000

    Added stopwords corpus

---
+++ trunk/dkpro.toolbox.corpus-asl/src/main/java/dkpro/toolbox/corpus/special/StopwordsCorpus.java
@@ -0,0 +1,132 @@
+package dkpro.toolbox.corpus.special;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import dkpro.toolbox.core.Sentence;
+import dkpro.toolbox.core.Tag;
+import dkpro.toolbox.core.Tag.Tagset;
+import dkpro.toolbox.core.TaggedToken;
+import dkpro.toolbox.core.Text;
+import dkpro.toolbox.corpus.Corpus;
+import dkpro.toolbox.corpus.CorpusException;
+
+public class StopwordsCorpus
+    implements Corpus
+{
+    public enum StopwordsLanguage {
+        danish,
+        dutch,
+        english,
+        finnish,
+        french,
+        german,
+        hungarian,
+        italian,
+        norwegian,
+        portuguese,
+        russian,
+        spanish,
+        swedish,
+        turkish
+    }
+    
+    private StopwordsLanguage language;
+    private List<String> tokens;
+    
+    public StopwordsCorpus(StopwordsLanguage language)
+            throws CorpusException
+    {
+        this.language = language;
+        
+        String file = "classpath:/corpus/stopwords/" + language.name() + ".txt.gz";
+        try {
+            tokens = readWords(file);
+        }
+        catch (IOException e) {
+            throw new CorpusException(e);
+        }
+    }
+    
+    private List<String> readWords(String path)
+            throws IOException
+    {     
+        URL url = ResourceUtils.resolveLocation(path, this, null);
+        InputStream is = url.openStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(is), "UTF-8"));
+        
+        List<String> tokens = new ArrayList<String>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            tokens.add(line);
+        }
+        return tokens;
+    }
+    
+    @Override
+    public Iterable<String> getTokens()
+        throws CorpusException
+    {
+        return tokens;
+    }
+
+    @Override
+    public Iterable<TaggedToken> getTaggedTokens()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Sentence> getSentences()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Tag> getTags()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Iterable<Text> getTexts()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+
+    @Override
+    public Tagset getTagset()
+        throws CorpusException
+    {
+        throw new CorpusException("Unavailable for this corpus.");
+    }
+    
+    @Override
+    public String getLanguage()
+    {
+        return language.name();
+    }
+
+    @Override
+    public String getName()
+    {
+        return "stopwords";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "stopwords lists of several languages";
+    }
+}
